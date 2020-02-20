df89aa3d5de87902f5981fa529d45db82a9b6360
Yifan Wu <ywu@lendingclub.com>
2020-02-20 12:29:27 -0800

    add get_text utilities

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	readme_description = f.read()
+
+setuptools.setup(
+	name="textractutil",
+	version="0.0.1",
+	author="Yifan Wu",
+	author_email="yw693@cornell.edu",
+	description="Python utilities for parsing AWS Textract results.",
+	long_description=readme_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.3",
+    install_requires=[
+        "pyyaml>=5.1,<=5.2"
+    ]
+)
+
---
+++ textractutil/__init__.py
@@ -0,0 +1,34 @@
+import os
+import logging
+import logging.config
+import yaml
+
+__name__ = "textractutil"
+__version__ = "0.0.1"
+
+import importlib
+
+from . import input_validator
+importlib.reload(input_validator)
+from . import parser
+importlib.reload(parser)
+
+"""
+Set up logging
+| Default logging configuration can be edited in logging.yaml.
+| You can also bring customized logging modules.
+"""
+
+env_key = "LOG_CFG"
+value = os.getenv(env_key, None)
+
+init_file_dir = os.path.dirname(os.path.abspath(__file__))
+logger_config_yaml_path = value if value else os.path.join(init_file_dir, "logging.yaml")
+logger_default_level = logging.INFO
+
+if os.path.exists(logger_config_yaml_path):
+	with open(logger_config_yaml_path, "rt") as f:
+		config = yaml.safe_load(f.read())
+	logging.config.dictConfig(config)
+else:
+	logging.basicConfig(level=logger_default_level)
